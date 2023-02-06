242cb66f6372b3f5d6ba1846591f7df7bbd4a616
Nathalie Albert <nathaliealbert@MacBook-Pro-de-Nathalie.local>
2023-02-06 16:15:08 +0100

    add filters + empty templates

---
+++ serv.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to our music tracker website !")
+	})
+
+	fmt.Println("Server is running on localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+func OpenFile(w http.ResponseWriter, r *http.Request, filename string) {
+	templates, err := template.ParseFiles("./templates/" + filename + ".html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	templates.Execute(w, templates)
+}
+func Hostler(w http.ResponseWriter, r *http.Request) {
+}
