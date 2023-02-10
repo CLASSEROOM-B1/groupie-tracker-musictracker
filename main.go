516eafb4e7b065b4cb6aca24ab4c2b53f75068ab
pierre <pierre.allain2003@gmail.com>
2023-02-10 16:05:17 +0100

    get artist

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"musicTracker/musicTracker"
+)
+
+func main() {
+	musicTracker.GetArtist("Queen")
+}
---
+++ musicTracker/getData.go
@@ -0,0 +1,20 @@
+package musicTracker
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func GetData(lien string) []byte {
+	data, err := http.Get("http://" + lien)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Body.Close()
+	body, err := ioutil.ReadAll(data.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return body
+}
