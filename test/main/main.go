314f2d9b4cfb67b4bd646643467a26a1eda61978
rach-Z <rachfane.ahmed@ynov.com>
2023-02-10 16:26:18 +0100

    icon

--- test/main/main.go
+++ test/main/main.go
@@ -7,58 +7,28 @@ import (
 	"net/http"
 )
 
-type APIrep struct {
-	Data []struct {
-		Id   int    `json:"Id"`
-		Name string `json:"Name"`
-	} //`json:"data"`
+type APIrep []struct {
+	Id   int    `json:"Id"`
+	Name string `json:"Name"`
+
+	//`json:"data"`
 }
 
-func GetApi(url string) (APIrep, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return APIrep{}, err
-	}
+func GetApi(url string) APIrep {
+	resp, _ := http.Get(url)
 	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return APIrep{}, err
-	}
-
+	body, _ := ioutil.ReadAll(resp.Body)
 	var response APIrep
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		return APIrep{}, err
-	}
+	json.Unmarshal(body, &response)
+	return response
 
-	return response, nil
-	// resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// defer resp.Body.Close()
-
-	// body, err := ioutil.ReadAll(resp.Body)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println(string(body))
 }
 
 func main() {
 	url := "https://groupietrackers.herokuapp.com/api/artists"
-	response, err := GetApi(url)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+	response := GetApi(url)
 
-	for _, data := range response.Data {
+	for _, data := range response {
 		fmt.Println("ID:", data.Id)
 		fmt.Println("NAME", data.Name)
 	}
